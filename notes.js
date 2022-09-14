fb7420fdabe34154e18a1f5d7c40872d53f9f286
David Benton <djb@dajabe.nz>
2022-09-14 16:34:54 +1200

    Add read and write functionality

--- notes.js
+++ notes.js
@@ -6,10 +6,27 @@ const client = new PocketBase('http://whau.haume.nz:8090')
 
 // fetch a paginated records list
 
-const resultList = async () => {
+// # node notes.js one two three four five
+// will result in
+// args:  [ 'one', 'two', 'three', 'four', 'five' ]
+const action = process.argv[2]
+const category = process.argv[3]
+const note = process.argv.slice(4).join(' ')
+
+const getNotes = async () => {
   return await client.records.getList('notes', 1, 50, {
     filter: 'created >= "2022-01-01 00:00:00"',
   })
+}
+
+const sendData = {
+  user: 'dr0bnbro24kif74',
+  category,
+  note,
+}
+
+const addNote = async (data) => {
+  return await client.records.create('notes', data)
 }
 
 // alternatively you can also fetch all records at once via getFullList:
@@ -21,4 +38,17 @@ const resultList = async () => {
 //   }
 // )
 
-resultList().then((res) => console.log(res))
+// getNotes().then((res) => console.log(res))
+
+switch (action) {
+  case 'add':
+    addNote(sendData)
+    break
+  default:
+    getNotes().then((noteList) => {
+      console.log('[Category] Note')
+      noteList.items.forEach((note) =>
+        console.log(`[${note.category}] ${note.note}`)
+      )
+    })
+}
